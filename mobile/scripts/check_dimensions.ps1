f76acb0e80f161ef4e2a323081665b25aea4970d
lean1835 <lean1835.vac@gmail.com>
2026-06-24 13:26:23 +0700

    ver2

--- mobile/scripts/check_dimensions.ps1
+++
@@ -1,4 +0,0 @@
-[System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") | Out-Null
-$img = [System.Drawing.Image]::FromFile("d:\Projects\Apptrololo\mobile\assets\images\renthome.png")
-Write-Output "$($img.Width)x$($img.Height)"
-$img.Dispose()
