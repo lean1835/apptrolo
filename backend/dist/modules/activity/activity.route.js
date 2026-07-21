ae15394e0920f10af6453023a0518990a5988a5c
lean1835 <lean1835.vac@gmail.com>
2026-07-21 10:07:28 +0700

    Untrack ignored node_modules and update git index

--- backend/dist/modules/activity/activity.route.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const activity_controller_1 = require("./activity.controller");
-const router = (0, express_1.Router)();
-router.get('/', activity_controller_1.getRecent);
-exports.default = router;
--- backend/dist/modules/data/data.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const data_controller_1 = require("./data.controller");
-const router = (0, express_1.Router)();
-router.get('/export', data_controller_1.exportData);
-router.post('/import', data_controller_1.importData);
-exports.default = router;
